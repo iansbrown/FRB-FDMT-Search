50e6a36fd1031962281c99dff1ad972a19fcac21
Ian Brown <ianbrown@uwm.edu>
2018-01-11 17:33:46 -0600

    added creation of event.txt file for list of detection events, Added test function to generate a test pulse, added option parser to add command line options -o obsid, -p path, and -test

--- v2_FDMT_search.py
+++ v2_FDMT_search.py
@@ -13,8 +13,38 @@ from astropy.io import fits
 import os
 import errno
 import timeit
+from optparse import OptionParser
+
+parser = OptionParser() #create parser for program options
+
+parser.add_option("-o", "--obsid",type='string',action="store", dest="obsid",help='obsid to be used')#obsid for hypercube
+parser.add_option('-p','--path',type='string', action= 'store', dest='path',help='path to creat fits files in', default='FDMT')#path to store FDMT output
+parser.add_option('-t','--test',type='string', action= 'store', dest='test',help='True to generate a test pulse', default=False)#set True to create a test pulse
+options, args = parser.parse_args()
 
 # define functions
+def Test():
+    '''generate a single pixel pulse to feed into FDMT for test purposes
+    '''
+    DM=500
+    df=.08
+    f0=138.594
+    dt=2
+    tmax=112
+
+    f=f0+(np.arange(768)*df) 
+
+    delay=(4.148808*((((f/1000)**-2)-(f.max()/1000)**-2)*DM))/1000
+
+    D=np.zeros(int((tmax/dt)),len(f))
+    D[np.int32(np.floor((delay/dt))),np.arange(len(f))]=1
+    nt,nf=np.shape(D)
+    D.shape=(nt,nf,1,1)
+    #plt.clf()
+    #plt.imshow(D.T,origin='lower',aspect=1,vmin=0,vmax=1,extent=(0,tmax,f.min(),f.max()))
+    return D
+    
+    
 def t_pulse(t_2, f_2, f, DM):
     """
     Function for generating the time values of a burst given the frequency
@@ -73,9 +103,10 @@ def calculate_SNR(row):
 
 #pos = [0, 256, 512, 768, 1024]
 counter = 0 # total number of detections
-path="default"
-obsid="1133102400"
+path=options.path
+obsid=options.obsid
 start = timeit.default_timer()
+
 '''
 for p in xrange(16):
     print p, dtime.datetime.now()
@@ -87,8 +118,13 @@ for p in xrange(16):
 # load data
 #filename = '1113366704_y'+str(top)+'-'+str(bottom)+'_x'+str(left)+'-'+str(right) # fill this in
 filepath = os.getcwd()#os.path.join(os.getcwd(),path,obsid) # fits file containing data with axes y,x,f,t
-with fits.open('%s/%sI.fits'%(filepath,obsid)) as hdulist:
-    sub_image = hdulist[0].data[:,:,:,:] # 8:520, 8:583
+
+if options.test == True:
+    sub_image=Test()
+    
+else:    
+    with fits.open('%s/%sI.fits'%(filepath,obsid)) as hdulist:
+        sub_image = hdulist[0].data[:,:,:,:] # 8:520, 8:583
 N_t, nf, N_x, N_y   = np.shape(sub_image) # nf is not power of two
 print (N_t,nf,N_x,N_y)
 # data parameters
@@ -118,7 +154,7 @@ sigma = 6.5 # S/N detection threshold
 y_lst, x_lst, SNR_lst, DM_lst, t_lst = [], [], [], [], [] # store detections
 tm = N_t*dt # for modulus purposes
 
-outdir = ('%s/aFDMT/'%(filepath))
+outdir = os.path.join(filepath,path)#('%s/aFDMT/'%(filepath))
 #create outdir if does not already exist.
 if not os.path.exists(os.path.dirname(outdir)):
     try:
@@ -178,58 +214,13 @@ for i in xrange(N_y):
                 plt.savefig(outdir+'event'+str(counter)+'_FDMT.png')
                 plt.close()
 
-'''outdir = ('%s/FDMT/'%(filepath)) # fill this in
-num = len(y_lst) # number of detections
-#create outdir if does not already exist.
-if not os.path.exists(os.path.dirname(outdir)):
-    try:
-        os.makedirs(os.path.dirname(outdir))
-    except OSError as exc: # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
-
-# save figures
-tm = N_t*dt # for modulus purposes
-print (num)
-for i in xrange(num):
-    img = image[:,:,x_lst[i],y_lst[i]]
-    plt.figure(figsize=(18,8))
-    plt.imshow(img, origin='lower', cmap='Greys_r', interpolation='nearest', \
-           extent=(t[0], t[-1]+dt, f_min-df/2., \
-                   f_max+df/2.), aspect='auto')
-    t_pmax = t_pulse(t_lst[i], f_min, f, DM_lst[i]) # superimpose
-    plt.plot((t_pmax + 5.*dt) % tm, f, 'r') # dispersion curve, shifted 5 bins to 
-    # the right to not block data in case it is hard to see, also modded
-    if t_lst[i] < N_f*dt: # need to show whole image to see modded part
-        plt.xlim(0., tm)
-    else:
-        plt.xlim(max(t_lst[i]-65.,0.), min(t_lst[i]+50.,tm)) # zoom in around pulse
-    plt.ylim(f_min-df/2., f_max+df/2.) # set limits or there will be white 
-    # space
-    plt.colorbar()
-    plt.xlabel('Time (s)', size=16)
-    plt.ylabel('Frequency (MHz)', size=16)
-    plt.title('%s,%s  S/N=%.2f'%((y_lst[i]),(x_lst[i]),SNR_lst[i]), size=18)
-    plt.savefig(outdir+'event'+str(counter+i)+'_curve.png')
-    plt.close()
-
-    #A = mod_FDMT(im)
-    plt.figure(figsize=(18,8))
-    plt.imshow(A, origin='lower', cmap='hot', interpolation='nearest', \
-           extent=(t[0], t[-1]+dt, DMs[0]-dDM/2., DMs[-1]+dDM/2.), \
-           aspect='auto')
-    plt.plot(t_lst[i]+0.5*dt, DM_lst[i], 'c*', markersize=8.)
-    plt.xlim(max(t_lst[i]-65.,0.), min(t_lst[i]+50.,tm))
-    plt.ylim(DMs[0]-dDM/2., DMs[-1]+dDM/2.)
-    plt.colorbar()
-    plt.xlabel('Time (s)', size=16)
-    plt.ylabel('Dispersion Measure (pc cm^-3)', size=16)
-    plt.title('%s,%s  S/N=%.2f'%((y_lst[i]),(x_lst[i]),SNR_lst[i]), size=18)
-    plt.savefig(outdir+'event'+str(counter+i)+'_FDMT.png')
-    plt.close()
-
-counter += num
-image = 0
-'''
+file1 = open(outdir+'events.txt','w')
+for l in xrange(len(y_lst)):
+    q=('%s,%s,%s,%s,%s\n'% (SNR_lst[l],DM_lst[l],t_lst[l],y_lst[l],x_lst[l]))
+    file1.write(q) 
+file1.flush()
+file1.close() 
+    
+    
 print (counter)
 print("total time for FDMT:%f")%(timeit.default_timer()-start)
